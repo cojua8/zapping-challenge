2595254a72f81443a86c4a947dd96ced8ff37fea
Joaquín Neira <github@joaquinneira.fastmail.com>
2024-08-22 02:45:14 +0000

    minimal hls

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 
 app.use(cors({ origin: "http://localhost:5500" }));
 
 app.get("/", (req, res) => {
   res.send({ result: "Hello World!" });
+});
+
+app.get("/video.m3u8", (req, res) => {
+  res.sendFile(path.join(__dirname, "videos", "segment.m3u8"));
+});
+
+app.get("/resources/:resource", (req, res) => {
+  console.log("requesting file", req.params.resource);
+  res.sendFile(path.join(__dirname, "videos", req.params.resource));
 });
 
 const port = 3000;
