65195cb190d1a31afd7a703f56b7d9710b0d5e58
Joaquín Neira <github@joaquinneira.fastmail.com>
2024-08-22 03:37:25 +0000

    in memory playlist

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,9 @@ app.get("/", (req, res) => {
 });
 
 app.get("/video.m3u8", (req, res) => {
-  res.sendFile(path.join(__dirname, "videos", "segment.m3u8"));
+  let file = createPlaylistFile();
+  res.header("Content-Type", "application/vnd.apple.mpegurl");
+  res.send(file);
 });
 
 app.get("/resources/:resource", (req, res) => {
@@ -22,3 +24,35 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Zapping HLS app listening on port ${port}`);
 });
+
+function createPlaylistFile() {
+  let sequenceCounter = 0;
+  let playlistFile =
+    createPlaylistHeaders(sequenceCounter) +
+    createSegment(sequenceCounter) +
+    createSegment(sequenceCounter + 1) +
+    createSegment(sequenceCounter + 2);
+
+  if (sequenceCounter + 2 === 63) {
+    playlistFile = playlistFile + "#EXT-X-ENDLIST";
+  }
+
+  return Buffer.from(playlistFile);
+}
+
+function createSegment(segmentCounter) {
+  let extinf =
+    segmentCounter === 63 ? "#EXTINF:4.566667,\n" : "#EXTINF:10.000000,\n";
+  let segmentFile = `http://localhost:3000/resources/segment${segmentCounter}.ts\n`;
+
+  return extinf + segmentFile;
+}
+
+function createPlaylistHeaders(sequenceCounter) {
+  return (
+    "#EXTM3U\n" +
+    "#EXT-X-VERSION:3\n" +
+    "#EXT-X-TARGETDURATION:10\n" +
+    `#EXT-X-MEDIA-SEQUENCE:${sequenceCounter}\n`
+  );
+}
