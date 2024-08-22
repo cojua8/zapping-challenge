a45d71f14ff5853cc75e5a07a2f09b2285552115
Joaquín Neira <github@joaquinneira.fastmail.com>
2024-08-22 02:42:23 +0000

    prettier

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-app.use(cors({origin: 'http://localhost:5500'}))
+app.use(cors({ origin: "http://localhost:5500" }));
 
-app.get('/', (req, res) => {
-  res.send({result: 'Hello World!'})
-})
+app.get("/", (req, res) => {
+  res.send({ result: "Hello World!" });
+});
 
-const port = 3000
+const port = 3000;
 app.listen(port, () => {
-  console.log(`Zapping HLS app listening on port ${port}`)
-})
+  console.log(`Zapping HLS app listening on port ${port}`);
+});
