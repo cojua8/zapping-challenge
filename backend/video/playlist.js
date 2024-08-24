dbf7dd4bb1d85482a31dddc23c7a4910a74ffc22
Joaquín Neira <github@joaquinneira.fastmail.com>
2024-08-24 18:13:30 +0000

    refactor and docstrings

--- backend/video/playlist.js
+++ backend/video/playlist.js
@@ -2,38 +2,22 @@ const constants = require("../constants");
 
 module.exports = createPlaylistFile;
 
-let startedTimestamp = Date.now();
-
+/**
+ * Creates an in-memory playlist file for the video stream
+ */
 function createPlaylistFile() {
   let segmentsCount = elapsedSegments();
 
-  let playlistFile = createPlaylistHeaders(segmentsCount);
-  for (let counter = 0; counter < constants.PLAYLIST_SIZE; counter++) {
-    playlistFile += createSegment(segmentsCount + counter);
-  }
+  let headers = createPlaylistHeaders(segmentsCount);
+  let segments = createPlaylistSegments(segmentsCount);
+  let footer = createPlaylistFooter(segmentsCount);
 
-  if (
-    segmentsCount ===
-    constants.TOTAL_SEGMENTS - constants.PLAYLIST_SIZE + 1
-  ) {
-    playlistFile = playlistFile + "#EXT-X-ENDLIST";
-  }
-
-  return Buffer.from(playlistFile);
+  return Buffer.from(headers + segments + footer);
 }
 
-function createSegment(segmentCounter) {
-  let segmentFile = `segment${segmentCounter}.ts\n`;
-
-  if (segmentCounter < constants.TOTAL_SEGMENTS) {
-    return "#EXTINF:10.000000,\n" + segmentFile;
-  } else if (segmentCounter === constants.TOTAL_SEGMENTS) {
-    return "#EXTINF:4.566667,\n" + segmentFile;
-  } else {
-    return "";
-  }
-}
-
+/**
+ * Creates the headers for the playlist
+ */
 function createPlaylistHeaders(sequenceCounter) {
   return (
     "#EXTM3U\n" +
@@ -43,9 +27,53 @@ function createPlaylistHeaders(sequenceCounter) {
   );
 }
 
+/**
+ * Create the segments of the playlist
+ */
+function createPlaylistSegments(segmentsCount) {
+  let segments = "";
+  for (let counter = 0; counter < constants.PLAYLIST_SIZE; counter++) {
+    segments += createSegment(segmentsCount + counter);
+  }
+  return segments;
+}
+
+/**
+ * Creates the footer for the playlist
+ * The footer is only present when the last segment is reached
+ */
+function createPlaylistFooter(segmentsCount) {
+  let footer = "";
+  if (segmentsCount >= constants.TOTAL_SEGMENTS - constants.PLAYLIST_SIZE + 1) {
+    footer = "#EXT-X-ENDLIST";
+  }
+  return footer;
+}
+
+/**
+ * Creates a segment for the playlist file
+ */
+function createSegment(segmentCounter) {
+  let segmentFile = `segment${segmentCounter}.ts`;
+
+  if (segmentCounter < constants.TOTAL_SEGMENTS) {
+    return `#EXTINF:10.000000,\n${segmentFile}\n`;
+  } else if (segmentCounter === constants.TOTAL_SEGMENTS) {
+    return `"#EXTINF:4.566667,\n${segmentFile}\n`;
+  } else {
+    return "";
+  }
+}
+
+// The timestamp when server started. Helps simulating live streaming
+let startedTimestamp = Date.now();
+/**
+ * Returns the number of segments (of ten seconds) that have elapsed since
+ * the server started, capped at the maximum number of segments plus one
+ */
 function elapsedSegments() {
   return Math.min(
     Math.trunc((Date.now() - startedTimestamp) / 10000),
-    constants.TOTAL_SEGMENTS
+    constants.TOTAL_SEGMENTS + 1
   );
 }
