b1ba65c7ce9add1b03127335330e676378025b92
Joaquín Neira <github@joaquinneira.fastmail.com>
2024-08-24 04:10:40 +0000

    user validation

--- backend/users.js
+++ backend/users.js
@@ -5,12 +5,36 @@ module.exports = usersRouter;
 
 usersRouter.use(express.json());
 
+users = {};
+
 usersRouter.post("/login", (req, res) => {
-  console.log("Logging in user", JSON.stringify(req.body));
-  res.send({});
+  console.log("Logging in user", req.body);
+  let { email, password } = req.body;
+
+  if (!users[email]) {
+    res.sendStatus(400);
+    return;
+  } else if (users[email].password !== password) {
+    res.sendStatus(400);
+    return;
+  } else {
+    let name = users[email].name;
+    res.send({ name, email });
+  }
 });
 
 usersRouter.post("/register", (req, res) => {
   console.log("Registering user", JSON.stringify(req.body));
-  res.send({});
+  let { name, email, password, confirmPassword } = req.body;
+
+  if (password !== confirmPassword) {
+    res.sendStatus(400);
+    return;
+  } else if (users[email]) {
+    res.sendStatus(400);
+    return;
+  } else {
+    users[email] = { password, name };
+    res.send({ name, email });
+  }
 });
