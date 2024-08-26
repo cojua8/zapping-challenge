0224194245efc6a56e0a7bfebda2125445820103
Joaquin Neira <joaquin.neira@houm.com>
2024-08-26 19:39:45 -0400

    use link

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,4 +1,5 @@
 import { FormProvider, useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const LoginForm = () => {
   const methods = useForm();
@@ -12,44 +13,44 @@ const LoginForm = () => {
   return (
     <FormProvider {...methods}>
       <form onSubmit={methods.handleSubmit(onSubmit)}>
-      <div className="form-floating mb-4">
-        <input
-          type="email"
-          className="form-control"
-          placeholder="my@email.com"
-          required
+        <div className="form-floating mb-4">
+          <input
+            type="email"
+            className="form-control"
+            placeholder="my@email.com"
+            required
             {...methods.register("email")}
-        />
-        <label htmlFor="email">Email</label>
-      </div>
-      <div className="form-outline form-floating mb-4">
-        <input
-          type="password"
-          className="form-control"
-          placeholder="******"
-          required
-            {...methods.register("password")}
-        />
-        <label htmlFor="password">Contraseña</label>
-      </div>
-      <div className="d-flex justify-content-between align-items-start flex-wrap gap-2">
-        <div className="d-flex gap-3 flex-wrap">
-          <button type="submit" className="btn btn-primary btn-block">
-            Iniciar sesión
-          </button>
-          <button
-            type="button"
-              onClick={fillForm}
-            className="btn btn-outline-success btn-block"
-          >
-            Llenar formulario
-          </button>
+          />
+          <label htmlFor="email">Email</label>
         </div>
-        <p>
-          ¿No eres miembro? <a href="signup.html">Regístrate</a>
-        </p>
-      </div>
-    </form>
+        <div className="form-outline form-floating mb-4">
+          <input
+            type="password"
+            className="form-control"
+            placeholder="******"
+            required
+            {...methods.register("password")}
+          />
+          <label htmlFor="password">Contraseña</label>
+        </div>
+        <div className="d-flex justify-content-between align-items-start flex-wrap gap-2">
+          <div className="d-flex gap-3 flex-wrap">
+            <button type="submit" className="btn btn-primary btn-block">
+              Iniciar sesión
+            </button>
+            <button
+              type="button"
+              onClick={fillForm}
+              className="btn btn-outline-success btn-block"
+            >
+              Llenar formulario
+            </button>
+          </div>
+          <p>
+            ¿No eres miembro? <Link to="/signup">Regístrate</Link>
+          </p>
+        </div>
+      </form>
     </FormProvider>
   );
 };
