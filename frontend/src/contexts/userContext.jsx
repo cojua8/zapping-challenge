d635d99d23935acc1aafcfd632144cb7c7439127
Joaquin Neira <joaquin.neira@houm.com>
2024-08-27 10:09:47 -0400

    persiste user

--- frontend/src/contexts/userContext.jsx
+++ frontend/src/contexts/userContext.jsx
@@ -1,9 +1,20 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const UserContext = createContext(undefined);
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : undefined;
+  });
+
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("user");
+    }
+  }, [user]);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
