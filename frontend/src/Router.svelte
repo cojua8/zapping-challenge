23cbaaad2e388f7e0e149ef6348cdb466fce6570
Joaquin Neira <joaquin.neira@houm.com>
2024-09-03 22:36:46 +0000

    routing

---
+++ frontend/src/Router.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { Route, Router } from "svelte-routing";
+    import Login from "./pages/Login.svelte";
+    import Player from "./pages/Player.svelte";
+    import Signup from "./pages/Signup.svelte";
+</script>
+
+<Router>
+    <Route path="/" component={Login} />
+    <Route path="/signup" component={Signup} />
+    <Route path="/player" component={Player} />
+</Router>
